a9836b4f270d87887859a563ea68fd3f62eec783
vritant <vritoku@gmail.com>
2016-12-24 18:23:19 -0500

    part 6 done

--- Color game/script.js
+++ Color game/script.js
@@ -1,10 +1,13 @@
-var colors = generateRandomColors(6);
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
 var h1 = document.querySelector("h1");
 var messageDisplay = document.getElementById('message');
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById('colorDisplay');
 colorDisplay.textContent = pickedColor;
 var resetButton = document.querySelector("#reset");
+var easyButton = document.querySelector('#easy');
+var hardButton = document.querySelector('#hard');
 
 
 var squares = document.getElementsByClassName('square');
@@ -29,10 +32,43 @@ for(var i = 0; i < squares.length; i++) {
   });
 }
 
+easyButton.addEventListener("click", function() {
+  easyButton.classList.add("selected");
+  hardButton.classList.remove("selected");
+  numSquares = 3;
+
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+
+  for(var i = 0; i < squares.length; i++) {
+    if(colors[i]) {
+      squares[i].style.background = colors[i];
+    }
+    else {
+      squares[i].style.display = "none";
+    }
+  }
+});
+
+hardButton.addEventListener("click", function() {
+  hardButton.classList.add("selected");
+  easyButton.classList.remove("selected");
+  numSquares = 6;
+
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+
+  for(var i = 0; i < squares.length; i++) {
+    squares[i].style.background = colors[i];
+    squares[i].style.display = "block";
+  }
+});
 
 resetButton.addEventListener("click", function() {
   //generate all new colors
-  colors = generateRandomColors(6);
+  colors = generateRandomColors(numSquares);
   //pick new random color from array
   pickedColor = pickColor();
   //change colorDisplay to match picked color
