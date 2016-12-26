04376cbbde0a9d0bd8f37e38c4241818aaa8b6db
vritant <vritoku@gmail.com>
2016-12-25 20:20:03 -0500

    part 4 done

--- ToDoApp/assets/js/script.js
+++ ToDoApp/assets/js/script.js
@@ -2,3 +2,11 @@
 $('li').click(function() {
   $(this).toggleClass("completed");
 });
+
+//click on X to delete To-DO
+$("span").click(function(event) {
+  $(this).parent().fadeOut(function() {
+    $(this).remove();
+  });
+  event.stopPropagation();
+});
