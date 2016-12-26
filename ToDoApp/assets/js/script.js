a01966892dc3afdaa8084821dc9e5fa6a323e7c7
vritant <vritoku@gmail.com>
2016-12-25 21:55:43 -0500

    part 5 done

--- ToDoApp/assets/js/script.js
+++ ToDoApp/assets/js/script.js
@@ -1,12 +1,24 @@
 // Check off specific ToDos by clicking
-$('li').click(function() {
+$('ul').on("click", "li", function() {
   $(this).toggleClass("completed");
 });
 
 //click on X to delete To-DO
-$("span").click(function(event) {
+$("ul").on("click", "span", function(event) {
   $(this).parent().fadeOut(function() {
     $(this).remove();
   });
   event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+  if (event.which === 13) {
+    //grab new To-Do text
+    var text = $(this).val();
+    //clear input
+    $(this).val("");
+    //create new li and add to ul
+    $("ul").append("<li><span>X</span> " + text + "</li>");
+
+  }
 });
