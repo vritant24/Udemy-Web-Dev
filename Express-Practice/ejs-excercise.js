2b431cf09b5ff7294b098147bbb4c1d815948c6a
vritant <vritoku@gmail.com>
2016-12-29 16:17:47 -0500

    learnt POST routing

--- Express-Practice/ejs-excercise.js
+++ Express-Practice/ejs-excercise.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+var friends = ["nobody", "no one"];
 
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
@@ -22,6 +25,15 @@ app.get("/posts", function(req, res) {
   res.render("posts", {posts: posts});
 });
 
+app.get("/friends", function(req, res) {
+  res.render("friends", {friends: friends});
+})
+
+app.post("/addfriend", function(req, res) {
+    var newFriend = req.body.newfriend;
+    friends.push(newFriend);
+    res.redirect("/friends");
+});
 
 var listener = app.listen(8000, function(){
   console.log("server has started at " + listener.address().port);
