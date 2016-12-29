916cccd29c83f8c7bd4da1b6ce9479d98dc6dfec
vritant <vritoku@gmail.com>
2016-12-29 12:43:04 -0500

    Doing good with ejs

---
+++ Express-Practice/ejs-excercise.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var app = express();
+
+app.get("/", function(req, res) {
+  res.render("home.ejs");
+});
+
+app.get("/bro/:name", function(req, res) {
+  var nameBro = req.params.name;
+  res.render("bro.ejs", {name: nameBro});
+});
+
+app.get("/posts", function(req, res) {
+  var posts = [
+    {title: "Post 1", author: "VB"},
+    {title: "Post 2", author: "BV"},
+    {title: "Post 3", author: "BB"}
+  ];
+  res.render("posts.ejs", {posts: posts});
+});
+
+
+var listener = app.listen(8000, function(){
+  console.log("server has started at " + listener.address().port);
+});
