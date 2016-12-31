f7e330be8f657e1dc5ef68e0e2db1244eeb79e80
vritant <vritoku@gmail.com>
2016-12-31 09:17:05 -0500

    starting yelp camp

--- API/app.js
+++ API/app.js
@@ -1,13 +1,19 @@
 var express = require("express");
 var app = express();
 var request = require('request');
+app.set("view engine", "ejs");
 
+app.get("/", function(req, res) {
+  res.render("search");
+});
 
 app.get("/results", function(req, res) {
-  request("http://www.omdbapi.com/?s=indiana", function (error, response, body) {
+  var results = req.query.search
+  var url = "http://www.omdbapi.com/?s=" + results;
+  request(url, function (error, response, body) {
     if(!error && response.statusCode == 200) {
       var data = JSON.parse(body);
-      res.send(data);
+      res.render("results", {data: data});
     }
   });
 });
