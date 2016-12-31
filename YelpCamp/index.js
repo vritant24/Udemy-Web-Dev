ae397b6eb2ab2eae9f1891c145c6d66526309e7a
vritant <vritoku@gmail.com>
2016-12-31 10:27:09 -0500

    added bootstrap styling and made it responsive

--- YelpCamp/index.js
+++ YelpCamp/index.js
@@ -4,6 +4,10 @@ var bodyParser = require("body-parser");
 var campgrounds = [
   {name: "Yosemite", image:"https://content-oars.netdna-ssl.com/wp-content/uploads/2015/12/Yosemite.Fran_.jpg"},
   {name: "Mavericks", image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT07x-BklL_YEC6LFNQKYgZ8Vq-jEPk6viePXr-3rAiUwpvdGAquw"},
+  {name: "Yosemite", image:"https://content-oars.netdna-ssl.com/wp-content/uploads/2015/12/Yosemite.Fran_.jpg"},
+  {name: "Mavericks", image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT07x-BklL_YEC6LFNQKYgZ8Vq-jEPk6viePXr-3rAiUwpvdGAquw"},
+  {name: "Yosemite", image:"https://content-oars.netdna-ssl.com/wp-content/uploads/2015/12/Yosemite.Fran_.jpg"},
+  {name: "Mavericks", image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT07x-BklL_YEC6LFNQKYgZ8Vq-jEPk6viePXr-3rAiUwpvdGAquw"},
   {name: "El Capitan", image:"http://www.divergentmedia.com/blog/wp-content/uploads/2015/09/Yosemites-El-Capitan-Mountain-Wall-Wallpaper.jpg"}
 ];
 
