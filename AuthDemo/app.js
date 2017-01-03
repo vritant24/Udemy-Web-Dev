cd8dd6c6aaf381ecc4c3c9dbb4dd4c1504a5d9e3
vritant <vritoku@gmail.com>
2017-01-03 11:51:42 -0500

    added login functiinality with passport

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -9,6 +9,7 @@ var express                 = require("express"),
 
 mongoose.connect("mongodb://localhost/auth_app_demo");
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(require("express-session")({
@@ -17,12 +18,38 @@ app.use(require("express-session")({
     saveUninitialized: false
 }));
 
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+//================
+//    ROUTES
+//================
 app.get("/", function(req, res) {
   res.render("home");
 });
 
 app.get("/secret", function(req, res) {
   res.render("secret");
+});
+
+//user sign up form
+app.get("/register", function(req, res) {
+  res.render("register");
+});
+
+//handle user sign up
+app.post("/register", function(req, res) {
+  User.register(new User({username: req.body.username}), req.body.password, function(err, user) {
+    if(err) {
+      console.log(err);
+      res.render("register");
+    } else {
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/secret");
+      });
+    }
+  });
 });
 
 var listener = app.listen(8000, function() {
