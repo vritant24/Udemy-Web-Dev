581faabe52997a0535e1f57b178fa2551d0b8b44
vritant <vritoku@gmail.com>
2017-01-05 20:25:19 -0500

    Created edit and update routes for comments

--- YelpCamp/routes/comments.js
+++ YelpCamp/routes/comments.js
@@ -43,6 +43,31 @@ router.post("/", isLoggedIn, function(req, res) {
   })
 });
 
+//EDIT ROUTE
+router.get("/:comment_id/edit", function(req, res) {
+  Comment.findById(req.params.comment_id, function(err, editedComment) {
+    if(err) {
+      console.log(err);
+      res.redirect("back");
+    } else {
+        res.render("comments/edit", {campground_id: req.params.id, comment: editedComment});
+    }
+  });
+});
+
+//UPDATE ROUTE
+router.put("/:comment_id", function(req, res) {
+  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment) {
+    if(err) {
+      console.log(err);
+      res.redirect("back");
+    } else {
+      res.redirect("/campgrounds/" + req.params.id);
+    }
+  });
+});
+
+
 //==================
 //     MIDDLEWARE
 //==================
